649a3b9f8d8dbd38b8b2fe10fbe540154e0024b6
byhj <476652422@qq.com>
2016-02-12 23:36:34 +0800

    add blending

---
+++ src/Blending/Cube.h
@@ -0,0 +1,44 @@
+#ifndef Triangle_H
+#define Triangle_H
+
+#include <gl/glew.h>
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "ogl/oglShader.h"
+
+namespace byhj
+{
+	class Cube
+	{
+	public:
+
+	Cube()  = default;
+	~Cube() = default;
+
+	void init();
+	void update();
+	void render();
+	void shutdown();
+
+	private:
+
+		void init_buffer();
+		void init_vertexArray();
+		void init_shader();
+
+	    GLuint m_program;
+	    GLuint m_vao;
+	    GLuint m_ibo;
+	    GLuint m_vbo;
+		GLuint u_model;
+		GLuint u_view;
+		GLuint u_proj;
+
+	   std::vector<GLfloat> m_VertexData;
+	   std::vector<GLuint>  m_IndexData;
+
+	   OGLShader m_CubeShader ={ "CubeShader" };
+	};
+}
+#endif
---
+++ src/Blending/oglRender.cpp
@@ -0,0 +1,45 @@
+ #include "oglRender.h"
+#include <memory>
+
+
+namespace byhj
+{
+
+	OGLRender::OGLRender()
+	{
+	}
+
+	OGLRender::~OGLRender()
+	{
+
+	}
+	
+
+	void OGLRender::v_init()
+	{
+        m_cube.init();
+	}
+
+	void OGLRender::v_update()
+	{
+		m_cube.update();
+	}
+
+    void OGLRender::v_render()
+	{
+		glEnable(GL_DEPTH_TEST);
+
+		static const GLfloat black[] ={ 0.2f, 0.3f, 0.4f, 1.0f };
+		glClearBufferfv(GL_COLOR, 0, black);
+		static const GLfloat one[] ={ 1.0f };
+		glClearBufferfv(GL_DEPTH, 0, one);
+
+		m_cube.render();
+
+    }
+	void OGLRender::v_shutdown()
+	{
+		m_cube.shutdown();
+	}
+
+}
