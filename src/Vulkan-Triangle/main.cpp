80fcda9f86f89d757a446fcd74b3579099c2e3cc
byhj <476652422@qq.com>
2016-02-27 16:29:31 +0800

    modifed vulkan triangle

--- src/Vulkan-Triangle/main.cpp
+++ src/Vulkan-Triangle/main.cpp
@@ -1,14 +1,38 @@
-#include <windows.h>
+#include "Triangle.h"
+#include <memory>
 
 #pragma comment(lib, "vulkan-1.lib")
 
+static std::shared_ptr<Triangle> pTriangle = std::make_shared<Triangle>();
+
 #ifdef _WIN32
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (pTriangle != NULL)
+	{
+		pTriangle->handleMessages(hWnd, uMsg, wParam, lParam);
+	}
+	return (DefWindowProc(hWnd, uMsg, wParam, lParam));
+}
+
+#else 
+
+static void handleEvent(const xcb_generic_event_t *event)
+{
+	if (vulkanExample != NULL)
+	{
+		vulkanExample->handleEvent(event);
+	}
+}
+#endif
 
 //Enable the console to output the debug message
 #ifdef _DEBUG
 #pragma comment( linker, "/subsystem:\"console\" /entry:\"WinMainCRTStartup\"")
 #endif
 
+#ifdef _WIN32
 int APIENTRY WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	                  _In_ LPSTR lpCmdLine, _In_ int nShowCmd )
 #else
@@ -16,6 +40,10 @@ int main(int argc, char **argv)
 #endif
 
 {
+	pTriangle->setupWindow(hInstance, WndProc);
+	pTriangle->initSwapchain();
+	pTriangle->init();
+	pTriangle->renderLoop();
 
 	return 0;
 }
---
+++ src/Vulkan-Triangle/triangle.cpp
@@ -0,0 +1,58 @@
+#include "Triangle.h"
+
+void Triangle::init()
+{
+	VulkanBase::prepare();
+
+	void buildCmdBuffers();
+	void initVertex();
+	void initPipeline();
+	void initUbo();
+	void initDescriptorPool();
+	void initDescriptorSet();
+	void initDescriptorSetLayout();
+
+	prepared = true;
+}
+
+void Triangle::update()
+{
+	void updateUbo();
+}
+
+void Triangle::render()
+{
+}
+
+void Triangle::shutdown()
+{
+}
+
+
+void Triangle::buildCmdBuffers()
+{
+}
+
+void Triangle::initVertex()
+{
+}
+void Triangle::initPipeline()
+{
+}
+void Triangle::initUbo()
+{
+}
+
+void Triangle::initDescriptorPool()
+{
+}
+void Triangle::initDescriptorSet()
+{
+}
+void Triangle::initDescriptorSetLayout()
+{
+}
+
+void Triangle::updateUbo()
+{
+}
