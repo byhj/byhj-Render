c7ce18377f809f1668810605e1855eb6ca0f175d
byhj <476652422@qq.com>
2016-03-24 16:28:18 +0800

    add water

---
+++ src/Water/main.cpp
@@ -0,0 +1,18 @@
+#include "oglRender.h"
+
+#include "root.h"
+
+// #ifdef _DEBUG
+// #include <vld.h>
+// #endif
+
+int main()
+{
+	auto oglApp      = std::make_shared<byhj::OGLRender>();
+
+	byhj::Root::getInstance()->setRender(oglApp);
+	byhj::Root::getInstance()->BeginScene();
+	byhj::Root::getInstance()->EndScene();
+
+	return 0;
+}
---
+++ src/Water/oglRender.h
@@ -0,0 +1,21 @@
+#ifndef OGLRender_H
+#define OGLRender_H
+#include "ogl/oglApp.h"
+
+namespace byhj
+{
+	class OGLRender : public OGLApp
+	{
+	public:
+		OGLRender();
+		~OGLRender();
+
+		void v_init();
+		void v_update();
+		void v_render();
+		void v_shutdown();
+
+	private:
+	};
+}
+#endif
