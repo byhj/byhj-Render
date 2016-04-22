4650ac10c54601c7b829a6fee7cf5a209cdcd56f
byhj <476652422@qq.com>
2016-04-22 14:40:22 +0800

    add d3d12-texture

---
+++ src/DirectX12-Texture/triangle.cpp
@@ -0,0 +1,155 @@
+#include "triangle.h"
+#include "d3d/d3d12DDSLoader.h"
+
+namespace byhj {
+
+	void Triangle::init(ComPtr<ID3D12Device> pD3D12Device)
+	{
+		init_buffer(pD3D12Device);
+		init_shader(pD3D12Device);
+	}
+
+	void Triangle::update()
+	{
+	}
+
+	void Triangle::render(ComPtr<ID3D12GraphicsCommandList> pCommandList)
+	{
+		pCommandList->SetDescriptorHeaps(1, m_pSamplerDescHeap);
+		pCommandList->SetGraphicsRootDescriptorTable(1, m_pSamplerDescHeap->GetGPUDescriptorHandleForHeapStart());
+		pCommandList->IASetVertexBuffers(VERTEX_BUFFER_SLOT, 1, &m_vertexBufferView);
+		pCommandList->DrawInstanced(VERTEX_NUM, 1, 0, 0);
+	}
+
+	void Triangle::shutdown()
+	{
+	}
+	ComPtr<ID3D12PipelineState> Triangle::getPipelineState()
+	{
+		return m_pPipelineState;
+	}
+
+	ComPtr<ID3D12RootSignature> Triangle::getRootSignature()
+	{
+		return m_pRootSignature;
+	}
+
+	/////////////////////////////////////////////////////////////
+	void Triangle::init_buffer(ComPtr<ID3D12Device> pD3D12Device) 
+	{
+		//Create Vertex Buffer 
+
+		Vertex VertexData[3] = {
+			{ {  0.0f,  0.5f, 0.0f }, { 1.0f, 0.0f, 0.0f, 1.0f } },
+			{ {  0.5f, -0.5f, 0.0f }, { 0.0f, 1.0f, 0.0f, 1.0f } },
+			{ { -0.5f, -0.5f, 0.0f }, { 0.0f, 0.0f, 1.0f, 1.0f } }
+		};
+		const UINT VertexBufferSize = sizeof(VertexData);
+
+		//Note: using upload heaps to transfer static data like vertex buffers is not recommended.
+		//Every time the GPU needs it, the upload heap will be marshalled over.
+		//Please read up on Default Heap usage. An upload heap is used here for code simplicity and
+		//because there are very few vertexs to atually transfer.
+		HRESULT res;
+		res = pD3D12Device->CreateCommittedResource(
+			           &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD), 
+			           D3D12_HEAP_FLAG_NONE,
+			           &CD3DX12_RESOURCE_DESC::Buffer(VertexBufferSize),
+			           D3D12_RESOURCE_STATE_GENERIC_READ,
+			           nullptr,
+			           IID_PPV_ARGS(&m_pVertexBuffer)
+			           );
+		assert(res == S_OK);
+
+		//Copy the triangle data to the vertex buffer
+		void *pVertexDataBegin = nullptr;
+		res = m_pVertexBuffer->Map(0, nullptr, &pVertexDataBegin);
+		memcpy(pVertexDataBegin, VertexData, VertexBufferSize);
+		m_pVertexBuffer->Unmap(0, nullptr);
+
+		//Initialize the vertex buffer view.
+		m_vertexBufferView.BufferLocation = m_pVertexBuffer->GetGPUVirtualAddress();
+		m_vertexBufferView.StrideInBytes  = sizeof(Vertex);
+		m_vertexBufferView.SizeInBytes    = VertexBufferSize;
+	}
+
+	void Triangle::init_shader(ComPtr<ID3D12Device> pD3D12Device)
+	{
+		//Create a root signature
+		CD3DX12_ROOT_SIGNATURE_DESC rootSignatureDesc;
+		rootSignatureDesc.Init(0, nullptr, 0, nullptr, D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+		ComPtr<ID3DBlob> signature;
+		ComPtr<ID3DBlob> error;
+		D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, &error);
+		pD3D12Device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&m_pRootSignature) );
+
+
+		//Create the pipeline state, which includes compiling and loading shaders
+		ComPtr<ID3DBlob> vertexShader;
+		ComPtr<ID3DBlob> pixelShader;
+#ifdef _DEBUG
+		// Enable better shader debugging with the graphics debugging tools.
+		UINT compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#else
+		UINT compileFlags = 0;
+#endif
+
+		D3DCompileFromFile(L"triangle.vsh", nullptr, nullptr, "VS_MAIN", "vs_5_0", compileFlags, 0, &vertexShader, nullptr);
+		D3DCompileFromFile(L"triangle.psh", nullptr, nullptr, "PS_MAIN", "ps_5_0", compileFlags, 0, &pixelShader, nullptr);
+		
+		// Define the vertex input layout.
+		D3D12_INPUT_ELEMENT_DESC inputElementDescs[] =
+		{
+			{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+			{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+		};
+
+		//Descrbe and create the graphics pipeline state object (PSO)
+		D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = {};
+		psoDesc.InputLayout = {inputElementDescs, _countof(inputElementDescs) };
+		psoDesc.pRootSignature = m_pRootSignature.Get();
+		psoDesc.VS = {vertexShader->GetBufferPointer(), vertexShader->GetBufferSize() };
+		psoDesc.PS = {pixelShader->GetBufferPointer(), pixelShader->GetBufferSize() };
+		psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+		psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+		psoDesc.DepthStencilState.DepthEnable = FALSE;
+		psoDesc.DepthStencilState.StencilEnable = FALSE;
+		psoDesc.SampleMask = UINT_MAX;
+		psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+		psoDesc.NumRenderTargets = 1;
+		psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+		psoDesc.SampleDesc.Count = 1;
+		HRESULT res = pD3D12Device->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&m_pPipelineState));
+		assert(res == S_OK);
+	}
+
+	void Triangle::init_texture(ComPtr<ID3D12Device> pD3D12Device) {
+
+		D3D12_RESOURCE_DESC resourceDesc = CD3DX12_RESOURCE_DESC::Tex2D(
+			DXGI_FORMAT_B8G8R8A8_UNORM, 256, 256, 1, 1, 1, 0, D3D12_RESOURCE_FLAG_NONE,
+			D3D12_TEXTURE_LAYOUT_UNKNOWN, 0);
+
+		pD3D12Device->CreateCommittedResource(
+			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+			D3D12_HEAP_FLAG_NONE,
+			&resourceDesc,
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(m_pTexture.ReleaseAndGetAddressOf()));
+
+		CreateDDSTextureFromFile(pD3D12Device, "d3d12.dds", m_pTexture, );
+		
+
+		D3D12_SAMPLER_DESC samplerDesc;
+		samplerDesc.Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
+		samplerDesc.AddressU = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
+		samplerDesc.AddressV = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
+		samplerDesc.AddressW = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
+		samplerDesc.MinLOD = -FLT_MAX;
+		samplerDesc.MaxLOD = FLT_MAX;
+		samplerDesc.MipLODBias = 0;
+		samplerDesc.MaxAnisotropy = 0;
+		samplerDesc.ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;
+		pD3D12Device->CreateSampler(&samplerDesc, m_pSamplerDescHeap->GetCPUDescriptorHandleForHeapStart());
+	}
+}
---
+++ src/DirectX12-Texture/triangle.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <d3d12.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <wrl.h>
+#include <assert.h>
+#include <allocators>
+#include "d3d/d3dx12.h"
+
+using namespace DirectX;
+using namespace Microsoft::WRL;
+
+namespace byhj 
+{
+	class Triangle {
+	public:
+		void init(ComPtr<ID3D12Device> pD3D12Device);
+		void update();
+		void render(ComPtr<ID3D12GraphicsCommandList> pCommandList);
+		void shutdown();
+
+		ComPtr<ID3D12PipelineState> getPipelineState();
+		ComPtr<ID3D12RootSignature> getRootSignature();
+
+		struct Vertex {
+			XMFLOAT3 pos;
+			XMFLOAT4 color;
+		};
+
+	private:
+		void init_buffer(ComPtr<ID3D12Device> pD3D12Device);
+		void init_shader(ComPtr<ID3D12Device> pD3D12Device);
+		void init_texture(ComPtr<ID3D12Device> pD3D12Device);
+
+		ComPtr<ID3D12PipelineState> m_pPipelineState;
+		ComPtr<ID3D12RootSignature> m_pRootSignature;
+		ComPtr<ID3D12Resource>      m_pVertexBuffer;
+		ComPtr<ID3D12Resource>      m_pTexture;
+		ComPtr<ID3D12DescriptorHeap> m_pSamplerDescHeap;
+		
+
+	    D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
+		const UINT VERTEX_NUM = 3;
+		const UINT VERTEX_BUFFER_SLOT = 0;
+
+	};
+}
